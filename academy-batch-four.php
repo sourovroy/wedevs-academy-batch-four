b20fcbb9957fb2d989520869718588bfb4712866
Sourov Chowdhury <schowdhury@awesomemotive.com>
2024-11-06 20:19:15 +0600

    Class of 6th november

--- academy-batch-four.php
+++ academy-batch-four.php
@@ -8,41 +8,74 @@ Author URI: https://sourov.xyz
 License: GPLv2
 License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Text Domain: academy-batch-four
-*/
+ */
 
 class ABFP_Academy_Batch_Four_Plugin {
 
-    private static $instance = null;
+	private static $instance = null;
 
-    private function __construct() {
-        add_filter( 'the_content', array( $this, 'academy_the_content' ) );
+	private function __construct() {
+		add_filter( 'the_content', array( $this, 'academy_the_content' ) );
 
-        add_action( 'wp_footer', 'ABFP_Academy_Batch_Four_Plugin::wp_footer' );
-    }
+		add_action( 'wp_footer', 'ABFP_Academy_Batch_Four_Plugin::wp_footer' );
+	}
 
-    public static function get_instance() {
-        if ( is_null( self::$instance ) ) {
-            self::$instance = new self();
-        }
+	public static function get_instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
 
-        return self::$instance;
-    }
+		return self::$instance;
+	}
 
-    public function academy_the_content( $content ) {
-        $url = get_the_permalink();
+	public function academy_the_content( $content ) {
+		$show = apply_filters( 'show_page_content_qr_code', true );
 
-        $new_content = '<img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . $url . '">';
+		$new_content = '';
 
-        return  $content . $new_content;
-    }
+		if ( $show ) {
+			$url = get_the_permalink();
 
-    public static function wp_footer() {
-        $url = home_url();
+			$new_content = '<img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . $url . '">';
+		}
 
-        $new_content = '<img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . $url . '">';
+		return $content . $new_content;
+	}
 
-        echo $new_content;
-    }
+	public static function wp_footer() {
+		do_action( 'before_footer_qr_code' );
+
+		$url = home_url();
+
+		$new_content = '<img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . $url . '">';
+
+		echo $new_content;
+	}
 }
 
 ABFP_Academy_Batch_Four_Plugin::get_instance();
+
+
+// Another Plugin
+add_filter( 'show_page_content_qr_code', function( $is_show ) {
+	$is_show = false;
+
+	return $is_show;
+} );
+
+function abfp_before_footer_qr_code() {
+	?>
+	<p>This is before QR Code.</p>
+	<?php
+}
+
+add_action( 'before_footer_qr_code', 'abfp_before_footer_qr_code', 20 );
+
+remove_action( 'before_footer_qr_code', 'abfp_before_footer_qr_code', 20 );
+
+function abfp2_before_footer_qr_code() {
+	?>
+	<p>This is before QR Code extra.</p>
+	<?php
+}
+add_action( 'before_footer_qr_code', 'abfp2_before_footer_qr_code', 20 );
